009278200068f17fa15b3225cd572efaf2ebccbf
Mateusz Michalak <michalakweb@gmail.com>
2019-04-03 16:29:26 +0200

    basic store is set

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {createStore} from 'redux';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        Calculator
       </div>
     );
   }
 }
 
+const calcReducer = (state = 0, action) => {
+  switch (action.type) {
+      case "INCREMENT":
+          return state + action.incrementBy;
+      default:
+          return state;
+  }
+}
+
+const store = createStore(calcReducer);
+
+console.log(store.getState());
+
+store.subscribe(() => console.log(store.getState()))
+
+const incrementAction = (incrementBy = 1) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+store.dispatch(incrementAction(5));
+
 export default App;
+
+// 1. ustawic sklep 
+// (ustawic reducer, 
+//   podpiac jedna akcje, 
+//   to wszystko pod sklep podpiac, 
+//   wyslac kilka akcji z programu)
