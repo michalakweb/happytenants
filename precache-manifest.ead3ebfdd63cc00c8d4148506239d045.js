5913c6fb0afe6e87bf8a07b9329841adeb06f7ec
Mateusz Michalak <michalakweb@gmail.com>
2019-04-09 17:09:51 +0200

    Updates

---
+++ precache-manifest.ead3ebfdd63cc00c8d4148506239d045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fed34a3ca22ddffd1a24",
+    "url": "/happytenants/static/css/main.eadbf786.chunk.css"
+  },
+  {
+    "revision": "fed34a3ca22ddffd1a24",
+    "url": "/happytenants/static/js/main.052141fa.chunk.js"
+  },
+  {
+    "revision": "4d23210ed51cc219c445",
+    "url": "/happytenants/static/js/runtime~main.3c5682d9.js"
+  },
+  {
+    "revision": "c7bdc9be974b3ae4f135",
+    "url": "/happytenants/static/css/2.4397b3cc.chunk.css"
+  },
+  {
+    "revision": "c7bdc9be974b3ae4f135",
+    "url": "/happytenants/static/js/2.b1623724.chunk.js"
+  },
+  {
+    "revision": "6889e9c3009a4566c0d2fadcad48ae68",
+    "url": "/happytenants/index.html"
+  }
+];
