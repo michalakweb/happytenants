0a93f38fdf017759066b025b87b8e3f50bd4a4d4
Mateusz Michalak <michalakweb@gmail.com>
2019-04-25 15:58:46 +0200

    removed facevook auth option

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -21,8 +21,7 @@ export const uiConfig = {
     signInFlow: 'popup',
     // We will display Google and Facebook as auth providers.
     signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
     ],
     callbacks: {
       // Avoid redirects after sign-in.
