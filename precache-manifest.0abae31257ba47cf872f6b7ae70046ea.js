a7b43abe594bd022d841f0ea19d6241f9b756da1
Mateusz Michalak <michalakweb@gmail.com>
2019-05-03 17:54:36 +0200

    Updates

--- precache-manifest.0abae31257ba47cf872f6b7ae70046ea.js
+++ precache-manifest.0abae31257ba47cf872f6b7ae70046ea.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "04230676f803127d3316",
+    "revision": "c68186c7d5b3e4a8a57e",
     "url": "/happytenants/static/css/main.9420e52e.chunk.css"
   },
   {
-    "revision": "04230676f803127d3316",
-    "url": "/happytenants/static/js/main.2b8347a6.chunk.js"
+    "revision": "c68186c7d5b3e4a8a57e",
+    "url": "/happytenants/static/js/main.fc9e43d9.chunk.js"
   },
   {
     "revision": "4d23210ed51cc219c445",
     "url": "/happytenants/static/js/runtime~main.3c5682d9.js"
   },
   {
-    "revision": "420bb38bd7df40e06ee1",
+    "revision": "1833a2377546d8c9c565",
     "url": "/happytenants/static/css/2.4397b3cc.chunk.css"
   },
   {
-    "revision": "420bb38bd7df40e06ee1",
-    "url": "/happytenants/static/js/2.6f4a8d0d.chunk.js"
+    "revision": "1833a2377546d8c9c565",
+    "url": "/happytenants/static/js/2.288397a1.chunk.js"
   },
   {
     "revision": "401a57a57fcff13cc692ad26c778144d",
     "url": "/happytenants/static/media/background.401a57a5.jpg"
   },
   {
-    "revision": "2c6d1efcc6a4ab4e12287a0a02f83b19",
+    "revision": "3c4dc3c64d74b5ef12be76ba24bd0701",
     "url": "/happytenants/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/happytenants/precache-manifest.c1a557ad9a2f15b36b7315ec1a881cf7.js"
+  "/happytenants/precache-manifest.0abae31257ba47cf872f6b7ae70046ea.js"
 );
 
 workbox.clientsClaim();
